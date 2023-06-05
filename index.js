0dc1203163054222008d59b41c36df34cb55432b
Shaikat roy <shaikatroy258@gmail.com>
2023-06-06 01:28:52 +0600

    payment intent added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY)
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -138,6 +139,13 @@ async function run() {
       res.send(result);
      })
 
+     app.delete('/menu/:id', verifyJWT, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
+     })
+
      // review related apis
      app.get('/reviews', async(req, res) =>{
       const result = await reviewCollection.find().toArray();
@@ -174,6 +182,20 @@ async function run() {
         const query = { _id: new ObjectId(id)};
         const result = await cartCollection.deleteOne(query);
         res.send(result);
+      })
+
+      // create payment intent 
+      app.post('/create-payment-intent', async(req, res) => {
+        const {price} = req.body;
+        const amount = price*100;
+        const paymentIntent = await AuthenticatorResponse.paymentIntents.create({
+          amount: amount,
+          currency: 'usd',
+          payment_method_types: ['card']
+        });
+        res.send({
+          clientSecret: paymentIntent.client_secret
+        })
       })
 
     // Send a ping to confirm a successful connection
