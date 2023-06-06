942d3153484bd1c03db7c2cf0709c95007cf9fec
Shaikat roy <shaikatroy258@gmail.com>
2023-06-06 15:24:15 +0600

    payment maethod added

--- index.js
+++ index.js
@@ -49,14 +49,15 @@ async function run() {
      const menuCollection = client.db("bistroDb").collection("menu");
      const reviewCollection = client.db("bistroDb").collection("reviews");
      const cartCollection = client.db("bistroDb").collection("carts");
+     const paymentCollection = client.db("bistroDb").collection("payments");
 
     app.post('/jwt', (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: '1h' 
       })
-
-            res.send({token});  
+      
+      res.send({token});  
     })
 
     // warning: use verifyJWT before using verifyAdmin
@@ -185,10 +186,11 @@ async function run() {
       })
 
       // create payment intent 
-      app.post('/create-payment-intent', async(req, res) => {
+      app.post('/create-payment-intent', verifyJWT, async(req, res) => {
         const {price} = req.body;
+
         const amount = price*100;
-        const paymentIntent = await AuthenticatorResponse.paymentIntents.create({
+        const paymentIntent = await stripe.paymentIntents.create({
           amount: amount,
           currency: 'usd',
           payment_method_types: ['card']
@@ -196,6 +198,17 @@ async function run() {
         res.send({
           clientSecret: paymentIntent.client_secret
         })
+      })
+
+      // payment related api
+      app.post('/payments', verifyJWT, async(req, res) => {
+        const payment = req.body;
+        const insertResult = await paymentCollection.insertOne(payment);
+         
+        const query = {_id: {$in: payment.cartItems.map(id => new ObjectId(id))}}
+        const deleteResult = await cartCollection.deleteMany(query)
+
+        res.send({ insertResult, deleteResult});
       })
 
     // Send a ping to confirm a successful connection
